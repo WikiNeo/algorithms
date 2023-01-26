71f683a757ae31c1c30d64abfcef28cd6db6a995
arch.wzy <mint.wzy@gmail.com>
2023-01-26 11:34:33 +0800

    begin to write some algorithm code again

--- python/src/graph/number-of-islands/number-of-islands.py
+++ python/src/graph/number-of-islands/number-of-islands.py
@@ -3,21 +3,28 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # get boundary values for 2-D problems first
         ROWS: int = len(grid)
         COLS: int = len(grid[0])
+
+        # stores visited coordinate
         visited = set()
+
+        # final result count
         count = 0
 
         def dfs(row: int, col: int) -> bool:
-            """return true if we can find an island starting from (row, col)"""
-            ...
+            """return true if we can find an island starting from (row, col)
 
-            # fail condition
-            if row < 0 or row >= ROWS or col < 0 or col >= COLS:
+            The side effect is that we will mark the visited location
+            """
+
+            # fail conditions
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS: # out of boudary
                 return False
-            if grid[row][col] == '0':
+            if grid[row][col] == '0':   # find water
                 return False
-            if (row, col) in visited:
+            if (row, col) in visited:   # visited already
                 return False
 
             # mark (row, col) as visited
@@ -31,9 +38,31 @@ class Solution:
 
             return True
 
+        # start from each location and check results
         for i in range(ROWS):
             for j in range(COLS):
                 if dfs(i, j):
                     count += 1
 
         return count
+
+
+if __name__ == '__main__':
+    grid1 = [
+      ["1","1","1","1","0"],
+      ["1","1","0","1","0"],
+      ["1","1","0","0","0"],
+      ["0","0","0","0","0"]
+    ]
+    solution1: Solution = Solution()
+    assert solution1.numIslands(grid1) == 1
+
+    grid2 = [
+      ["1","1","0","0","0"],
+      ["1","1","0","0","0"],
+      ["0","0","1","0","0"],
+      ["0","0","0","1","1"]
+    ]
+    solution2: Solution = Solution()
+    assert solution2.numIslands(grid2) == 3
+

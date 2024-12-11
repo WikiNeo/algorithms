66e9591f8cf0c4400454ff6625e45975ac1ed717
Wang Ziyi <mint.wzy@gmail.com>
2024-12-11 10:45:28 +0800

    migrate LargestComponent from blog here

---
+++ typescript/src/Graph/BFS/LargestComponent/LargestComponennt.ts
@@ -0,0 +1,88 @@
+interface Graph {
+  [key: string]: number[]
+}
+
+// ------------------ BFS Queue --------------------------------------
+const largestComponentBFS = (graph: Graph) => {
+  const visited = new Set<number>()
+  let largestSize = 0;
+
+  for (let node in graph) {
+    const size = countComponentSizeBFS(graph, node, visited)
+    if (size > largestSize) {
+      largestSize = size;
+    }
+  }
+
+  return largestSize;
+};
+
+const countComponentSizeBFS = (graph: Graph, src: string, visited: Set<number>) => {
+  // the base case should also return the size
+  if (visited.has(parseInt(src))) return 0;
+
+  let size = 0;
+  // add src to queue & visited
+  const queue = [src]
+  visited.add(parseInt(src))
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    if (current === undefined) continue;
+    size += 1;
+
+    for (let neighbor of graph[current]) {
+      if (visited.has(neighbor)) continue;
+      // if not visited, add it
+      visited.add(neighbor)
+      queue.push(String(neighbor))
+    }
+  }
+
+  return size;
+}
+
+
+console.log("------------BFS Queue-------")
+console.log(largestComponentBFS({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2]
+}) === 4);
+
+console.log(largestComponentBFS({
+  1: [2],
+  2: [1, 8],
+  6: [7],
+  9: [8],
+  7: [6, 8],
+  8: [9, 7, 2]
+}) === 6);
+
+console.log(largestComponentBFS({
+  3: [],
+  4: [6],
+  6: [4, 5, 7, 8],
+  8: [6],
+  7: [6],
+  5: [6],
+  1: [2],
+  2: [1]
+}) === 5);
+
+console.log(largestComponentBFS({}) === 0)
+
+console.log(largestComponentBFS({
+  0: [4, 7],
+  1: [],
+  2: [],
+  3: [6],
+  4: [0],
+  6: [3],
+  7: [0],
+  8: []
+}) === 3);
---
+++ typescript/src/Graph/BFS/LargestComponent/README.md
@@ -0,0 +1,14 @@
+# [Graph] Largest Component
+
+## Problem
+
+Write a function `largestComponent` that takes in the adjacency list of an
+undirected graph. The function should return the size of the largest connected
+component in the graph.
+
+## Thoughts
+
+When it comes to largest, we will always have a comparison logic.
+
+We need get the size of each components with DFS or BFS, then compare and
+return.

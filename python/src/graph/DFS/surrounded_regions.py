bc45d4d3290f3d1b83fc919d9e7ec45c35807266
Wang Ziyi <mint.wzy@gmail.com>
2024-12-28 12:18:51 +0800

    add surrounded regions

---
+++ python/src/graph/DFS/surrounded_regions.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        ROW, COL = len(board), len(board[0])
+
+        def capture(row, col):
+            """mark cells starting from (row, col) as 'T'"""
+            if row not in range(ROW) or col not in range(COL) or board[row][col] != "O":
+                return
+
+            board[row][col] = "T"
+            capture(row, col - 1)
+            capture(row - 1, col)
+            capture(row, col + 1)
+            capture(row + 1, col)
+
+        # mark all connected cells starting from edge as 'T'
+        for r in range(ROW):
+            for c in range(COL):
+                if (r == 0 or r == ROW - 1 or c == 0 or c == COL - 1) and board[r][
+                    c
+                ] == "O":
+                    capture(r, c)
+
+        # mark remaining 'O' cells as 'X'
+        for r in range(ROW):
+            for c in range(COL):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"

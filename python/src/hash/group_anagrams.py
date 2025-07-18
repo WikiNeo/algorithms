af753b402c2508137b362e01a17bb7e824dc7b05
WikiNeo <mint.wzy@gmail.com>
2025-07-18 22:18:18 +0800

    update group anagram

--- python/src/hash/group_anagrams.py
+++ python/src/hash/group_anagrams.py
@@ -14,3 +14,12 @@ class Solution:
             count_to_values[tuple(count)].append(s)
 
         return list(count_to_values.values())
+
+    def groupAnagrams2(self, strs: List[str]) -> List[List[str]]:
+        str_to_values = defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+            str_to_values[key].append(s)
+
+        return list(str_to_values.values())
